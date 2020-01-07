ebb158d8e125cfdeecbead478e37813b022ca965
fiopwk <flopwiki@gmail.com>
2020-01-07 15:49:35 -0500

    added axios import to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import { ImageList } from "src/ImageList.js";
+import axios from "axios";
 
 
 function App() {
-  return (
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+
+    axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=CWMpwYsCugMG5FPhzRMRaWklHLe3WzYhMQlklR2s')
+        .then( res => {
+            setImages(res.data);
+            console.log(res)
+        })
+        .catch( err => {
+            console.log(err.message)
+        });
+    }, []);
+
+ return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label="rocket">🚀!</span>
       </p>
         <div>
-            <ImageList/>
+            <h1>{images.title}</h1>
+            {/*image here below*/}
+            <img src="{images.url}" alt=""/>
+            <img src="https://apod.nasa.gov/apod/image/2001/IC405hp_ColesHelm_960.jpg" alt=""/>
+
+            {/*<p>{images.copyright}</p>*/}
+            <p>{images.date}</p>
+            
+        </div>
+        <div>
+            <p>{images.explanation}</p>
+
         </div>
     </div>
   );
