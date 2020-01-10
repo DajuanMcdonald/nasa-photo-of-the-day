293630c564707fa46777eaef3acecdd9a2d3f2ff
fiopwk <flopwiki@gmail.com>
2020-01-10 08:49:46 -0500

    attempt at stretch for DAY_TWO project

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./ImageCard.css";
-import {Button, Card, CardBody, CardImg, CardText, CardTitle, CardSubtitle} from "reactstrap";
+import {Card, CardBody, Col, CardImg, CardSubtitle, CardText, CardTitle, Nav, NavLink} from "reactstrap";
+import Button from "@material-ui/core/Button";
 import Alert from "reactstrap/lib/Alert";
 // import ButtonDropdown from "reactstrap/es/ButtonDropdown";
 
 export default function CreateCard() {
     const [images, setImages] = useState({});
+
     useEffect(() => {
         axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
             .then( res => {
@@ -19,35 +21,37 @@ export default function CreateCard() {
 
     return (
         <div className="App">
-            <div className="container-nav"><nav> || || || <a href="">||</a> <a href="">||</a></nav></div>
-                <p>
-                <span role="img" aria-label="rocket">🚀</span>
-                </p>
+            <div className="container-nav"><Nav> || || || <NavLink href="">||</NavLink> <a href="">||</a></Nav></div>
+                <div>
+                <Button variant="contained" color="primary">🚀</Button>
+                </div>
                 <h2>NASA's Photo of the Day</h2>
                 <Card>
                     {/*broken image*/}
                     <CardBody className="imageContainer">
 
-                        <div className="imgCard">
+                        <Col className="imgCard">
 
-                            <img src={images.hdurl} alt={images.title}/>
+                            <CardImg src={images.hdurl} alt={images.title}/>
                             <CardTitle className="title">{images.title}</CardTitle>
-                        </div>
+                            <Button variant="contained" color="secondary">{images.date}</Button>
+                        </Col>
 
                          <div>
-                         <span>{images.data}</span>
+                             <Alert>{images.copyright}</Alert>
+                             <>
 
-                        <CardText className="explain">{images.explanation}</CardText>
+                                <CardSubtitle className="text-info">{images.explanation}</CardSubtitle>
+
+                             </>
+
 
                     </div>
 
                      </CardBody>
 
-                    <h3>Date: </h3>
-                    <Alert>{images.copyright}</Alert>
 
 
-                    <Button color="primary" size="lg">{images.date}</Button>
 
 
                 </Card>
