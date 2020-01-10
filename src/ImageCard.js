7d6dcbae3dec3a8424c630a1ec8cf935bd72c4f3
fiopwk <flopwiki@gmail.com>
2020-01-09 23:32:27 -0500

    imported more reactstrap components

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,17 +1,17 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./ImageCard.css";
-import {Button} from "reactstrap";
+import {Button, Card, CardBody, CardImg, CardText, CardTitle, CardSubtitle} from "reactstrap";
 import Alert from "reactstrap/lib/Alert";
 // import ButtonDropdown from "reactstrap/es/ButtonDropdown";
 
-export default function CreateCard(props) {
+export default function CreateCard() {
     const [images, setImages] = useState({});
     useEffect(() => {
         axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
             .then( res => {
                 setImages(res.data);
-                console.log(props);
+                console.log(res.data);
 
 
             }).catch( err => {console.log(err.message);})
@@ -20,37 +20,37 @@ export default function CreateCard(props) {
     return (
         <div className="App">
             <div className="container-nav"><nav> || || || <a href="">||</a> <a href="">||</a></nav></div>
-            <p>
+                <p>
                 <span role="img" aria-label="rocket">🚀</span>
-            </p>
-            <h2>NASA's Photo of the Day</h2>
-            <div>
-                {/*broken image*/}
-                <div className="imageContainer">
+                </p>
+                <h2>NASA's Photo of the Day</h2>
+                <Card>
+                    {/*broken image*/}
+                    <CardBody className="imageContainer">
 
-                    <div className="imgCard">
+                        <div className="imgCard">
 
-                        <img src={images.hdurl} alt={images.title}/>
-                        <h1 className="title">{images.title}</h1>
-                    </div>
+                            <img src={images.hdurl} alt={images.title}/>
+                            <CardTitle className="title">{images.title}</CardTitle>
+                        </div>
 
-                     <div>
+                         <div>
                          <span>{images.data}</span>
 
-                        <p className="explain">{images.explanation}</p>
+                        <CardText className="explain">{images.explanation}</CardText>
 
                     </div>
 
-                </div>
+                     </CardBody>
 
-                <h3>Date: </h3>
-                <Alert>{images.copyright}</Alert>
-
-
-                <Button color="primary" size="lg">{images.date}</Button>
+                    <h3>Date: </h3>
+                    <Alert>{images.copyright}</Alert>
 
 
-            </div>
+                    <Button color="primary" size="lg">{images.date}</Button>
+
+
+                </Card>
 
         </div>
 
